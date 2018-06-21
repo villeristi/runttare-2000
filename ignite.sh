7ae3c859709e1e338fc9eeb7658c8f3b4acbdb03
Ville Ristimäki <villeristimaki@gmail.com>
2018-06-21 12:07:01 +0300

    Add ignite

--- ignite.sh
+++ ignite.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker build ./nginx -t runttare_nginx --no-cache && docker run -d -p 80:80 -v `pwd`/ui/dist:/app  runttare_nginx:latest
+# Lé Docker magig'n shit
+
+set -ex \
+    docker build ./nginx -t runttare_nginx --no-cache &&  \
+    docker build ./api -t runttare_api --no-cache &&  \
+    docker run -d -p 80:80 -v `pwd`/ui/dist:/app runttare_nginx:latest && \
+    docker run -d -p 3001:3001 runttare_api:latest
