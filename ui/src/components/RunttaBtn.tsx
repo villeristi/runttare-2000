a5b64dc33d9d06be82f23736113f58c6aad0276c
Wille Ristimäki <villeristimaki@gmail.com>
2023-10-27 15:35:23 +0300

    feat(ui): use nats.ws on ui

--- ui/src/components/RunttaBtn.tsx
+++ ui/src/components/RunttaBtn.tsx
@@ -4,16 +4,15 @@ import classNames from 'classnames';
 interface IButton {
   ready: boolean;
   processing: boolean;
-  loading: boolean;
   handleClick: (event: any) => void;
 }
 
-export default function RunttaBtn({ready, processing, loading, handleClick}: IButton) {
+export default function RunttaBtn({ready, processing, handleClick}: IButton) {
   return (
-    <div className={classNames('btn', {'active': processing || loading}, {'ready': ready})} onClick={handleClick}>
+    <div className={classNames('btn', {'active': processing}, {'ready': ready})} onClick={handleClick}>
       <div className="inner-wrapper">
         {ready ? (
-          <i className="material-icons-round">{processing || loading ? 'update' : 'add'}</i>
+          <i className="material-icons-round">{processing ? 'update' : 'add'}</i>
         ) : (
           <i className="material-icons-round">hourglass_empty</i>
         )}
